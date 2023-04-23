935863914f405990bfcb1ab523dc80d4b8928b8e
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2023-04-22 20:00:07 -0600

    leftside-bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React, {useState} from "react"
 import "./App.css"
 import Header from "./components/Header/Header"
+import TopRepos from "./components/TopRepos/TopRepos"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="Body">
-        <div className="Body__left">
-          <h1>HOLA</h1>
-        </div>
+        <aside className="Body__left">
+          <TopRepos />
+        </aside>
         <div className="Body__middle">
           <h1>HOLA</h1>
         </div>
